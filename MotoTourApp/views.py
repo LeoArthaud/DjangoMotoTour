88c7b5389ea02173454922c9d97d3754e592047c
DaceMoria <leo.arthaud@epsi.fr>
2020-02-06 21:01:05 +0100

    add(views.py):create index method

--- MotoTourApp/views.py
+++ MotoTourApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import PostForm
+from .models import Post
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+
+# Index
+class IndexView(ListView):
+    template_name='MotoTourApp/index.html'
+    context_object_name = 'post_list'
+    def get_queryset(self):
+        return Post.objects.all()
